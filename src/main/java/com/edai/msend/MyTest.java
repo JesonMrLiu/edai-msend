8301cde0bd1fa106c1fe385ae7a4b9a144404258
liugang <liugang@szhtxx.com>
2018-09-17 21:42:17 +0800

    提交测试

--- src/main/java/com/edai/msend/MyTest.java
+++ src/main/java/com/edai/msend/MyTest.java
@@ -1,9 +1,16 @@
 package com.edai.msend;
 
+import java.text.DecimalFormat;
+
 public class MyTest {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!!");
+		DecimalFormat fmt = new DecimalFormat("#0.0#%");
+		System.out.println(fmt.format(0.2233));
+		
+		System.out.println(fmt.format(34.555));
+		
 	}
 	
 }
